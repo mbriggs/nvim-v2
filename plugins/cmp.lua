2f365c2239de6de9d059017e9ed2b8d293f9bca5
Matt Briggs <matt@mattbriggs.net>
2022-05-04 06:55:37 -0700

    accept completion with ret

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -2,8 +2,15 @@ local cmp = require("cmp")
 
 return {
 	mapping = {
-		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
 		["<Tab>"] = cmp.config.disable,
 		["<S-Tab>"] = cmp.config.disable,
+		["<ESC>"] = function(fallback)
+			if cmp.visible() then
+				cmp.abort()
+			else
+				fallback()
+			end
+		end,
 	},
 }
