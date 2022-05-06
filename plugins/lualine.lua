efdc594df3647319e064925ccf5f34e94e650939
Matt Briggs <matt@mattbriggs.net>
2022-05-06 06:55:55 -0700

    remove active lsp

--- plugins/lualine.lua
+++ plugins/lualine.lua
@@ -1,14 +1,17 @@
 return function(config)
-  local filename = {
-    "filename",
-    file_status = true, -- Displays file status (readonly status, modified status)
-    path = 1,
+	local filename = {
+		"filename",
+		file_status = true, -- Displays file status (readonly status, modified status)
+		path = 1,
 
-    shorting_target = 100,
-  }
+		shorting_target = 100,
+	}
 
-  table.insert(config.sections.lualine_c, 1, filename)
-  table.insert(config.inactive_sections.lualine_c, 1, filename)
+	table.insert(config.sections.lualine_c, 1, filename)
+	table.insert(config.inactive_sections.lualine_c, 1, filename)
 
-  return config
+	-- remove active lsps, takes too much space
+	table.remove(config.sections.lualine_x, 2)
+
+	return config
 end
