f3ec222d3d9a35ec26ecbc1ab997f9e7b2c958a6
Matt Briggs <matt@mattbriggs.net>
2022-05-20 07:22:33 -0700

    null-ls now lives in plugins

---
+++ plugins/null-ls.lua
@@ -0,0 +1,43 @@
+-- Formatting and linting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim
+local status_ok, null_ls = pcall(require, "null-ls")
+if not status_ok then
+	return
+end
+
+local formatting = null_ls.builtins.formatting
+local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
+
+return {
+	debug = false,
+	sources = {
+		formatting.prettierd,
+		formatting.cmake_format,
+		formatting.shfmt,
+		formatting.sqlformat,
+		formatting.stylua,
+
+		diagnostics.shellcheck,
+		diagnostics.codespell,
+		diagnostics.eslint_d,
+		diagnostics.hadolint,
+		diagnostics.proselint,
+		diagnostics.golangci_lint,
+
+		code_actions.eslint_d,
+		code_actions.shellcheck,
+		code_actions.refactoring,
+	},
+	-- NOTE: You can remove this on attach function to disable format on save
+	on_attach = function(client)
+		print("save")
+		if client.server_capabilities.documentFormattingProvider then
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				desc = "Auto format before save",
+				pattern = "<buffer>",
+				callback = vim.lsp.buf.formatting_sync,
+			})
+		end
+	end,
+}
