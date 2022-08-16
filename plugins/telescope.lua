c9543dee24808b0af9817b377ff6e57f37ef4369
Matt Briggs <matt@mattbriggs.net>
2022-08-16 11:04:32 -0700

    esc closes telescope

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
 				["<C-p>"] = actions.move_selection_previous,
 				["<C-j>"] = actions.cycle_history_next,
 				["<C-k>"] = actions.cycle_history_prev,
+				["<ESC>"] = actions.close,
 			},
 			-- for normal mode
 			n = {
