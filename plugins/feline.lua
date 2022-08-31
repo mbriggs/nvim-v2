8e32810a2e52d0fb18a524d8ca68144c3584de47
Matt Briggs <matt@mattbriggs.net>
2022-08-31 11:14:24 -0700

    remove lsp names from statusbar

--- plugins/feline.lua
+++ plugins/feline.lua
@@ -1,6 +1,8 @@
 return function(config)
 	local provider = require("core.status").provider
 	local components = config.components.active[1]
+	local rcomponents = config.components.active[2]
+
 	-- insert name into statusline
 	table.insert(components, 4, { provider = provider.spacer(2) })
 	table.insert(components, 5, {
@@ -13,5 +15,9 @@ return function(config)
 	})
 	table.remove(components, 7) -- remove ft
 	table.remove(components, 7) -- remove ft spacer
+
+	table.remove(rcomponents, 2) -- remove lsp names
+	table.remove(rcomponents, 2) -- remove lsp names spacer
+
 	return config
 end
