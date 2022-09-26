7be276d4a3801d1cf832237a7a3e48fea23bbc3b
Matt Briggs <matt@mattbriggs.net>
2022-09-25 21:19:50 -0700

    allow ft mappings to be anything

--- ft_wk.lua
+++ ft_wk.lua
@@ -5,12 +5,11 @@ local ft_map = {}
 vim.cmd("autocmd FileType * lua SetWKBuffer()")
 function SetWKBuffer()
 	local ft = vim.api.nvim_buf_get_option(0, "filetype")
-	local opts = { prefix = "<leader>", buffer = 0, mode = "n" }
+	local opts = { buffer = 0 }
 	local map = ft_map[ft]
 
 	if map ~= nil then
-		map["name"] = "+Local"
-		wk.register({ [","] = map }, opts)
+		astronvim.which_key_register(map, opts)
 	end
 end
 
